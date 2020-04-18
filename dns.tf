6089c3db5bd19412fb4c5cffa90c87d52bb5d9b3
Matt Bell <matt@mbell.me>
2020-04-18 14:24:54 +0100

    Use cloudflare for DNS

--- dns.tf
+++ dns.tf
@@ -1,10 +1,21 @@
-data "digitalocean_domain" "bell_wtf" {
-  name = "bell.wtf"
+provider "cloudflare" {
+  version = "~> 2.0"
+  email   = "${var.cloudflare_email}"
+  api_key = "${var.cloudflare_api_key}"
 }
 
-resource "digitalocean_record" "craft" {
-  domain = data.digitalocean_domain.bell_wtf.name
-  type   = "A"
-  name   = "craft"
-  value  = module.minecraft.ipv4_address
+data "cloudflare_zones" "bell_wtf" {
+  filter {
+    name   = "bell.wtf"
+    status = "active"
+    paused = false
+  }
+}
+
+resource "cloudflare_record" "craft_bell_wtf" {
+  zone  = lookup(data.cloudflare_zones.bell_wtf.zones[0], "id")
+  name  = "craft"
+  type  = "A"
+  value = module.minecraft.ipv4_address
+  ttl   = var.dns_record_ttl
 }
--- variables.tf
+++ variables.tf
@@ -2,10 +2,23 @@ variable "do_token" {
   description = "DigitalOcean API token"
 }
 
+variable "cloudflare_email" {
+  description = "Email address for cloudflare"
+}
+
+variable "cloudflare_api_key" {
+  description = "API key for cloudflare"
+}
+
 variable "mc_server_key" {
   description = "Minecraft server private key"
 }
 
 variable "mc_server_key_pub" {
   description = "Minecraft server public key"
+}
+
+variable "dns_record_ttl" {
+  description = "Default TTL for DNS records"
+  default     = 3600
 }
