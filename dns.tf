f60a75203e51c42988cb8d44b75617c030f793d0
Matt Bell <matt@mbell.me>
2020-04-18 14:27:11 +0100

    Fix up parameters

--- dns.tf
+++ dns.tf
@@ -1,7 +1,7 @@
 provider "cloudflare" {
   version = "~> 2.0"
-  email   = "${var.cloudflare_email}"
-  api_key = "${var.cloudflare_api_key}"
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
 }
 
 data "cloudflare_zones" "bell_wtf" {
@@ -13,9 +13,9 @@ data "cloudflare_zones" "bell_wtf" {
 }
 
 resource "cloudflare_record" "craft_bell_wtf" {
-  zone  = lookup(data.cloudflare_zones.bell_wtf.zones[0], "id")
-  name  = "craft"
-  type  = "A"
-  value = module.minecraft.ipv4_address
-  ttl   = var.dns_record_ttl
+  zone_id = lookup(data.cloudflare_zones.bell_wtf.zones[0], "id")
+  name    = "craft"
+  type    = "A"
+  value   = module.minecraft.ipv4_address
+  ttl     = var.dns_record_ttl
 }
