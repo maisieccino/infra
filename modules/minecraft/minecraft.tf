61e28b36beba1bc7d6d5afd3f971846f50376c07
Matt Bell <matt@mbell.me>
2020-04-19 15:51:49 +0100

    Modify whitelist

--- modules/minecraft/minecraft.tf
+++ modules/minecraft/minecraft.tf
@@ -1,3 +1,8 @@
+locals {
+  region = "lon1"
+}
+
+
 data "template_file" "user_data" {
   template = file("${path.module}/files/userdata.yaml")
   vars = {
@@ -9,7 +14,7 @@ data "template_file" "user_data" {
 resource "digitalocean_droplet" "mc_server" {
   image      = "ubuntu-18-04-x64"
   name       = "mc"
-  region     = "lon1"
+  region     = local.region
   size       = "s-1vcpu-2gb"
   backups    = true
   monitoring = true
@@ -29,6 +34,24 @@ resource "digitalocean_droplet" "mc_server" {
     private_key = var.mc_server_key
   }
 }
+
+# Volume definition and attachment.
+resource "digitalocean_volume" "mc_server" {
+  region                  = local.region
+  name                    = "serverdata"
+  size                    = 30
+  initial_filesystem_type = "ext4"
+  description             = "Minecraft server volume"
+  tags = [
+    "minecraft"
+  ]
+}
+
+resource "digitalocean_volume_attachment" "foobar" {
+  droplet_id = digitalocean_droplet.mc_server.id
+  volume_id  = digitalocean_volume.mc_server.id
+}
+
 
 resource "digitalocean_floating_ip" "mc_server" {
   droplet_id = digitalocean_droplet.mc_server.id
