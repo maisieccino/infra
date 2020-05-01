7bad62ed55f7491c79e9f36a1d592193d238955a
Matt Bell <matt@mbell.me>
2020-05-01 20:53:11 +0100

    Remove minecraft

--- main.tf
+++ main.tf
@@ -4,24 +4,24 @@ provider "digitalocean" {
 }
 
 provider "cloudflare" {
-  version = "~> 2.0"
-  email   = var.cloudflare_email
+  version   = "~> 2.0"
+  email     = var.cloudflare_email
   api_token = var.cloudflare_api_token
 }
 
-// Minecraft server and related DNS record.
-
-module "minecraft" {
-  source            = "./modules/minecraft"
-  mc_server_key     = var.mc_server_key
-  mc_server_key_pub = var.mc_server_key_pub
-  dns_name          = "craft.bell.wtf"
-  le_email          = var.le_email
-}
-
-resource "digitalocean_record" "craft_bell_wtf" {
-  domain = digitalocean_domain.bell_wtf.id
-  type   = "A"
-  name   = "craft"
-  value  = module.minecraft.lb_ip
-}
+# // Minecraft server and related DNS record.
+#
+# module "minecraft" {
+#   source            = "./modules/minecraft"
+#   mc_server_key     = var.mc_server_key
+#   mc_server_key_pub = var.mc_server_key_pub
+#   dns_name          = "craft.bell.wtf"
+#   le_email          = var.le_email
+# }
+#
+# resource "digitalocean_record" "craft_bell_wtf" {
+#   domain = digitalocean_domain.bell_wtf.id
+#   type   = "A"
+#   name   = "craft"
+#   value  = module.minecraft.lb_ip
+# }
