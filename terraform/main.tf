b8a9b2061550abf7fea58fdb20634139a266717e
Maisie Bell <4870168+hiMaisie@users.noreply.github.com>
2022-04-11 22:07:40 +0100

    Tidy up files

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,30 +18,3 @@ provider "google" {
   project     = var.gcp_project
   region      = var.gcp_region
 }
-
-// K8s cluster.
-
-provider "kubernetes" {
-  host             = module.cluster.api_endpoint
-  token            = module.cluster.api_token
-  cluster_ca_certificate = base64decode(
-    module.cluster.ca_certificate
-  )
-}
-
-# // Minecraft server and related DNS record.
-#
-# module "minecraft" {
-#   source            = "./modules/minecraft"
-#   mc_server_key     = var.mc_server_key
-#   mc_server_key_pub = var.mc_server_key_pub
-#   dns_name          = "craft.bell.wtf"
-#   le_email          = var.le_email
-# }
-#
-# resource "digitalocean_record" "craft_bell_wtf" {
-#   domain = digitalocean_domain.bell_wtf.id
-#   type   = "A"
-#   name   = "craft"
-#   value  = module.minecraft.lb_ip
-# }
