6e62bf67f93b1c20829379030c079c50cb85e545
Maisie Bell <4870168+hiMaisie@users.noreply.github.com>
2022-04-11 21:58:58 +0100

    Fix versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,16 +1,23 @@
 terraform {
   required_providers {
     digitalocean = {
+      source = "digitalocean/digitalocean"
       version = "~> 1.16"
     }
     cloudflare = {
+      source = "cloudflare/cloudflare"
       version   = "~> 2.0"
     }
     github = {
+      source = "hashicorp/github"
       version      = "2.4.0"
     }
     tls = {
+      source = "hashicorp/tls"
       version = "2.2.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
     }
   }
   required_version = ">= 1.1.0"
