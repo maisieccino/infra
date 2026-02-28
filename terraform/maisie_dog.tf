b8f863f0f7fb14b1acbef6bbdf96ac8ec67421b1
Maisie Bell <4870168+maisieccino@users.noreply.github.com>
2026-02-28 18:18:26 +0000

    add auth domain (#14)

---
+++ terraform/maisie_dog.tf
@@ -0,0 +1,20 @@
+
+resource "cloudflare_zone" "maisie_dog" {
+  zone = "maisie.dog"
+}
+
+resource "cloudflare_record" "atproto_mbell_dog" {
+  zone_id = cloudflare_zone.maisie_dog.id
+  name = "_atproto"
+  type = "TXT"
+  ttl = var.dns_record_ttl
+  value = "did=did:plc:y3hux44nccewwyrdd3tachy3"
+}
+
+resource "cloudflare_record" "login_maisie_dog" {
+  name = "login.maisie.dog"
+  zone_id = cloudflare_zone.maisie_dog.id
+  type = "CNAME"
+  ttl = var.dns_record_ttl
+  value = "maisie-cd-34lpwpjxh6evhyjn.edge.tenants.eu.auth0.com"
+}
